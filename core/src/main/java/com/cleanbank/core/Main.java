05dd192e6bc9c274984acd92066ed329af980565
Pavel <IP696@mail.ru>
2019-10-06 14:14:32 +0300

    add core module

--- core/src/main/java/com/cleanbank/core/Main.java
+++ core/src/main/java/com/cleanbank/core/Main.java
@@ -1,7 +1,7 @@
-package com.cleanbank;
+package com.cleanbank.core;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Start");
+        System.out.println("Start core");
     }
 }
